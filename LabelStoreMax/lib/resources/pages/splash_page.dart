cb63c7c447a04c7f927b6c6634489db05f14cba4
franpaolocrispy <francesco.dellaquila@crispybacon.it>
2024-10-14 21:59:03 +0200

    simplify splash

--- LabelStoreMax/lib/resources/pages/splash_page.dart
+++ LabelStoreMax/lib/resources/pages/splash_page.dart
@@ -1,15 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_app/bootstrap/helpers.dart';
 import 'package:flutter_app/resources/pages/home_page.dart';
 import 'package:flutter_app/resources/widgets/buttons.dart';
 import 'package:flutter_app/utils/app_version/app_version_check.dart';
 import 'package:flutter_app/utils/remote_config_manager.dart';
-import 'package:flutter_app/utils/video_manager.dart';
 import 'package:nylo_framework/nylo_framework.dart';
-import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatefulWidget {
   static String path = "/splash";
@@ -19,21 +16,14 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  VideoPlayerController? _controller;
   bool _checkIsAppLocked = false;
-  bool _isSplashCompleted = false;
 
   _navigateToHome() {
-    _isSplashCompleted = true;
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
     routeTo(HomePage.path, navigationType: NavigationType.pushReplace, pageTransition: PageTransitionType.fade);
   }
 
-  _checkVideos() async {
-    await _initializeVideoPlayer();
-
-    Future.delayed(Duration(seconds: 5), () {
-      if (_isSplashCompleted || _checkIsAppLocked) return;
+  _initTimer() async {
+    Future.delayed(Duration(seconds: 1), () {
       _navigateToHome();
     });
   }
@@ -43,7 +33,7 @@ class _SplashScreenState extends State<SplashScreen> {
     _checkIsAppLocked = await AppVersionCheck.checkAppVersion();
 
     if (_checkIsAppLocked) {
-      showModalBottomSheet(
+      return showModalBottomSheet(
           context: context,
           builder: (BuildContext context) => Padding(
                 padding: EdgeInsets.all(24),
@@ -74,40 +64,15 @@ class _SplashScreenState extends State<SplashScreen> {
                 ),
               ));
     }
+
+    _initTimer();
   }
 
   @override
   void initState() {
     super.initState();
 
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     _checkAppVersion();
-    _checkVideos();
-  }
-
-  Future<void> _initializeVideoPlayer() async {
-    try {
-      File? videoFile = await VideoManager().getRandomVideo();
-      if (videoFile == null) {
-        print("Error loading video: video file is null");
-        return;
-      }
-
-      _controller = VideoPlayerController.file(videoFile)
-        ..initialize().then((_) {
-          setState(() {});
-          _controller?.setVolume(0);
-          _controller?.play();
-        });
-    } catch (e) {
-      print("Error loading video: $e");
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
   }
 
   @override
@@ -118,17 +83,15 @@ class _SplashScreenState extends State<SplashScreen> {
       body: GestureDetector(
         onTap: () => _navigateToHome(),
         child: Center(
-          child: _controller != null && _controller!.value.isInitialized
-              ? VideoPlayer(_controller!)
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(width: 196, height: 196, "public/assets/app_icon/appicon.png"),
-                    SizedBox(height: 4),
-                    CircularProgressIndicator(),
-                  ],
-                ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(width: 196, height: 196, "public/assets/app_icon/appicon.png"),
+              SizedBox(height: 4),
+              CircularProgressIndicator(),
+            ],
+          ),
         ),
       ),
     );
