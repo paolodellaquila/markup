17c2f6e20b990ddcec5755752dbdfcde9ce0a72b
franpaolocrispy <francesco.dellaquila@crispybacon.it>
2025-01-21 17:54:43 +0100

    feat(cr): minor fix

--- LabelStoreMax/lib/utils/language_utility.dart
+++ LabelStoreMax/lib/utils/language_utility.dart
@@ -35,6 +35,7 @@ class LanguageUtility {
   // Method to check if the user is outside Italy
   bool get isOutsideItaly {
     String? countryCode = getUserCountryCode();
+    if (countryCode == null) return false;
     return countryCode != 'IT';
   }
 }
